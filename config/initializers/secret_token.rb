a3dd07a38e808065f209bcbeddc9cbd997c61f29
Christof Spies <christof.spies@seips.net>
2011-09-13 13:29:28 +0200

    rails 3.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RulkMail::Application.config.secret_token = 'ab1b2990c88c59ee1958d18de9e71c4ec8244b0b6aad68b5c6516d5c575730cef1ad68be96cd08d0a756ff9d3e413c36f660dfd0c5ec89c92ffe3c8e5610f98e'
