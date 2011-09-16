e5ee2887e85df2682f18a8cd23836aecb45974fc
Christof Spies <christof.spies@seips.net>
2011-09-16 16:08:21 +0200

    add field upload to model and views

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,4 +1,7 @@
 class Campaign < ActiveRecord::Base
   validates :name, :presence => true,
                    :length => { :minimum => 5 }
+  mount_uploader :data, DataUploader
+  mount_uploader :template_html, TemplateHtmlUploader
+  mount_uploader :template_text, TemplateTextUploader
 end
